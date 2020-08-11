558f8081180f83bc5f62d29483242bfdd594c6fc
Leandro Klinger <Leandro.klingeroliveira@gmail.com>
2020-08-11 16:17:26 -0300

    OrderStatus enum

---
+++ src/main/java/com/leandroklinger/course/entities/enums/OrderStatus.java
@@ -0,0 +1,30 @@
+package com.leandroklinger.course.entities.enums;
+
+public enum OrderStatus {
+
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPEED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code=code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+	
+	public static OrderStatus valueOf(int cod) {
+		for(OrderStatus value : OrderStatus.values()) {
+			if(value.getCode() == cod) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus Code");
+	}
+	
+} 
