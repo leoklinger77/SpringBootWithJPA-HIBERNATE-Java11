7c81e9f980827218318d1c325096cf65f2f45418
Leandro Klinger <Leandro.klingeroliveira@gmail.com>
2020-08-12 20:49:04 -0300

    Exception handling - delete

---
+++ src/main/java/com/leandroklinger/course/sevices/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package com.leandroklinger.course.sevices.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
